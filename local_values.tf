d5b283326f4cc5f14922ddbe9142832cb8cb27af
corehello <corehello@gmail.com>
2020-06-07 12:02:56 +0800

    add local values

---
+++ local_values.tf
@@ -0,0 +1,46 @@
+locals {
+  string = "string"
+  number = 1
+  bool   = true
+}
+
+output "local_string" {
+  value = local.string
+}
+
+output "local_number" {
+  value = local.number
+}
+
+output "loca_bool" {
+  value = local.bool
+}
+locals {
+  list_string = ["item1", "item2", "item3"]
+  set_string  = ["item1", "item2", "item3"]
+  map_string = {
+    "key1" = "value1"
+    "key2" = 1
+    "key3" = false
+  }
+  object = {
+    key1 = "aoeu"
+    key2 = 0
+  }
+}
+
+output "local_list_string" {
+  value = local.list_string
+}
+
+output "local_set_string" {
+  value = local.set_string
+}
+
+output "local_map_string" {
+  value = local.map_string
+}
+
+output "local_object" {
+  value = local.object
+}
