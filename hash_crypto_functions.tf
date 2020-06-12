f05eb239292c724a88f541f4aa772bf447d770b5
corehello <corehello@gmail.com>
2020-06-12 21:45:02 +0800

    add remaining funcitons

--- hash_crypto_functions.tf
+++ hash_crypto_functions.tf
@@ -42,3 +42,58 @@ output "filesha512" {
   value       = filesha512("${path.module}/terraform.tf")
   description = "input -> file -> sha512 -> output, file content only UTF-8 text"
 }
+
+output "md5" {
+  value = md5("hello world")
+}
+
+/*
+resource "tls_private_key" "tpk" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+  provisioner "local-exec" {
+    command = "echo -n ${tls_private_key.tpk.public_key_pem} > public.key && echo -n 'hello world' | openssl rsautl -encrypt -pubin -inkey public.key > ciphertext"
+  }
+}
+
+output "rsadecrypt" {
+  value = rsadecrypt(filebase64("${path.module}/ciphertext"), tls_private_key.tpk.private_key_pem)
+}
+*/
+
+output "sha1" {
+  value = sha1("hello world")
+}
+
+output "sha256" {
+  value = sha256("hello world")
+}
+
+output "sha512" {
+  value = sha512("hello world")
+}
+
+output "uuid" {
+  value       = uuid()
+  description = "RFC 4122 seciton 4.4, Verison 4 UUID"
+}
+
+output "uuidv5_dns" {
+  value = uuidv5("dns", "www.terraform.io")
+}
+
+output "uuidv5_url" {
+  value = uuidv5("url", "https://www.terraform.io/")
+}
+
+output "uuidv5_oid" {
+  value = uuidv5("oid", "1.3.6.1.4")
+}
+
+output "uuidv5_x500" {
+  value = uuidv5("x500", "CN=Example,C=GB")
+}
+
+output "uuidv5_raw_ns" {
+  value = uuidv5("743ac3c0-3bf7-4a5b-9e6c-59360447c757", "LIBS:diskfont.library")
+}
