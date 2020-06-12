37ff41fe5d26a075835fb175c7e040a528281782
corehello <corehello@gmail.com>
2020-06-12 22:14:01 +0800

    add required_verions section

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,10 @@
 terraform {
   # ~> here means verison >= 0.12, < 0.13
   required_version = "~> 0.12"
+  required_providers {
+    tls = ">= 2.1.1"
+    dns = {
+      version = ">= 0.1.1"
+    }
+  }
 }
